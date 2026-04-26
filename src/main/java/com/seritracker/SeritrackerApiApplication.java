86168a7bd4385c574e2f2708adc1e48072bb6ce4
Anselmo Ortega <lfkanselmo@gmail.com>
2026-04-26 13:56:08 -0500

    feat: configuracion inicial de proyecto spring boot

---
+++ src/main/java/com/seritracker/SeritrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.seritracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeritrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeritrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seritracker/SeritrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seritracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeritrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
